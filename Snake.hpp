94904227972dc536b36ec9969ea622165cb162ee
Oriel Avraham <oriela@mobileye.com>
2024-06-22 22:22:29 +0300

    Adding documentation

--- Snake.hpp
+++ Snake.hpp
@@ -32,20 +32,76 @@ public:
   void deleteTail();
   void addHead(Position newHeadPosition);
   
-  // Returns False if move is illegal and true otherwise
+  /**
+  * @brief move<Direction>
+  * 
+  * Moves the snake according to Direction.
+  * 
+  * 
+  * @return true if movement is legal and false otherwise.
+  */
   bool moveUp();
   bool moveDown();
   bool moveRight();
   bool moveLeft();
+
+  /**
+  * @brief implements move<direction>
+  * @return true if movement is legal and false otherwise.
+  */
   bool move(Direction d);
 
+  /**
+  * @brief Check if position p is a legitimate position for a snake head.
+  * @param p Position being checked.
+  * @return true if position is legit and false otherwise.
+  */
   bool isLegitPosition(Position p);
+
+  /**
+  * @brief Check if any snake part is at position (x,y).
+  * @param x Row coordinate.
+  * @param y Column coordinate.
+  * @return true if snake is positioned in (x,y) and false otherwise.
+  */
   bool isSnakeAt(int x, int y) const;
+
+  /**
+  * @brief Check if any snake part is at position p.
+  * @param p Position to check.
+  * @return true if snake is positioned in p and false otherwise.
+  */
   bool isSnakeAt(Position p) const;
+
+  /**
+  * @brief This function is used to return the head/body mark of the snake.
+  * @param p Position of the snake.
+  * @return The character that represents the snake at position p.
+  */
   char getSnakeMark(Position p) const;
+
+
+  /**
+  * @brief Sets foodPosition with p.
+  */
   void setFoodPosition(Position p);
+
+  /**
+  * @brief Checks if a new direction is legitimate according to the current direction.
+  * @param newDirection New direction to check.
+  * @param currDirection Current direction the snake is heading.
+  * @return True if the new direction is legitimate and false otherwise.
+  */
   bool isLegitDirection(Direction newDirection, Direction currDirection) const;
+
+  /**
+  * @return eatLastMove, which says whether the snake ate the food in its last movement.
+  */
   bool didEatLastMove() const;
+
+  /**
+  * @return The length of the snake whole body.
+  */
   int getLength() const;
 
   friend class SnakeGame;
--- SnakeGame.hpp
+++ SnakeGame.hpp
@@ -15,14 +15,34 @@ private:
   
   Snake snake;
   Position foodPosition;
-  std::unordered_map<Position, isLocated, pair_hash> emptySpots;
+
+  /**
+  * @return A random position on the board.
+  */
   Position getPosition() const;
 public:
   SnakeGame(int boardDimension, double gameSpeed);
+  
+  /**
+  * @brief Set food on a clear board position.
+  */
   void setFoodPosition();
+  
   bool isGameOver() const;
+
+  /**
+  * @brief  Displays the board with the snake and food.
+  */
   void displayBoard() const;
+
+  /**
+  * @brief Gets a move request from the user, and changes the direction of the snake accordingly.
+  */
   void getMoveInput();
+
+  /**
+  * @brief Moves the snake according to the user's last movement request.
+  */
   void moveSnake();
 };
 
